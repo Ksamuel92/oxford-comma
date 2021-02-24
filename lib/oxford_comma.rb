a7841bdefe2164ca7ff92ed5a17525e77c4cb779
Kyle Samuel <Ksamuel92@gmail.com>
2021-02-24 12:32:15 -0500

    commit tree 583159915c8816ce8b8b7f708859632bed00b5ed

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+ beginning_array=array[0,2]
+    if (array.length == 1)
+        array.join
+    elsif(array.length == 2)
+        array.join(" and ")
+    elsif(array.length == 3)
+        beginning_array.join(", ") + ", and " + array.pop
+    else 
+        array[0, array.length - 1].join(", ") + ", and " + array.pop
+        end
 end
+
